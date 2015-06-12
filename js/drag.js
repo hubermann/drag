3c3def476ea95dbbab994b3c6186f2512270de0e
huber <huber@hubers-MacBook-Pro.local>
2015-06-12 18:18:25 -0300

    menu ok

--- js/drag.js
+++ js/drag.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-	$('li').draggable({ containment: 'document', revert: true, placeholder:'separar', 
+	
+	$('li').draggable({ revert: true, 
 		start: function(){
 		contenido = $(this).data('name');
 		//$('.recibido').addClass('separar'); // leagrego un margen para que sea comodo poner en medio 
 		}
 	});
 
-	$('#receptor').droppable({placeholder:'separar',accept: '.item', drop: function(){
+	$('#receptor').droppable({placeholder:'.separar',accept: '.item', drop: function(){
 		$('.recibido').removeClass('separar'); //quito el margen que mostraba para agregar un item
 		$('#receptor').append( '<li class="recibido"><img src="imgs/'+contenido+'.jpg" width="100%" /></li>' );
 		console.log(contenido);
-	}});
+	}
+	
+	});
 
 });
 
